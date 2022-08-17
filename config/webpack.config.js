7596bdc751a982e3db816eb4842213384521238d
Alvaro <alvarolukmanto@gmail.com>
2022-08-17 12:52:27 +0700

    enable webpack alias

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -318,7 +318,9 @@ module.exports = function (webpackEnv) {
           'react-dom$': 'react-dom/profiling',
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
-        ...(modules.webpackAliases || {})
+        ...(modules.webpackAliases || {}),
+        '@': path.resolve(__dirname, '../src'),
+        'assets': path.resolve(__dirname, '../src/assets')
       },
       plugins: [
         // Prevents users from importing files from outside of src/ (or node_modules/).
