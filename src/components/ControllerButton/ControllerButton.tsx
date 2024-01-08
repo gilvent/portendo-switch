d9a949964bd137d64fe0ac2c93535bc23c423aa7
Alvaro <alvarolukmanto@gmail.com>
2024-01-08 20:40:36 +0800

    feature - change controller animation approach
    prepare for easier route controlled transition

--- src/components/ControllerButton/ControllerButton.tsx
+++ src/components/ControllerButton/ControllerButton.tsx
@@ -1,68 +1,85 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useRef } from 'react';
 import styles from './ControllerButton.module.scss';
 import ControllerButtonContext from '@/context/ControllerButtonContext';
-import useControllerAnimations from './useControllerAnimations.hook';
 
 function ControllerButton() {
   const { actions } = useContext(ControllerButtonContext);
-  const { toDetachedConModeAnimation, toSingleConModeAnimation } =
-    useControllerAnimations();
-
-  useEffect(() => {
-    toDetachedConModeAnimation.current?.play();
-  }, []);
+  const rootRef = useRef<any>(null);
 
   return (
-    <div data-anim-target="controller-button" className={styles.controller}>
-      <div
-        data-anim-target="controller-group"
-        className={styles['btn-group-top']}
-      >
-        <button
-          data-anim-target="button-y"
-          className={`${styles['btn-joycon-1']} ${styles['btn-down']}`}
-          onClick={() => {
-            actions?.current?.['onControlYClick']();
-          }}
-        >
-          <div className={styles.arrow}></div>
-        </button>
-        <button
-          data-anim-target="button-x"
-          className={`${styles['btn-joycon-1']} ${styles['btn-up']}`}
-          onClick={() => {
-            actions?.current?.['onControlXClick']();
-          }}
-        >
-          <div className={styles.arrow}></div>
-        </button>
-      </div>
+    <div
+      ref={rootRef}
+      data-anim-target="controller-button"
+      className={styles.controller}
+    >
+      <div data-anim-target="tendo" className={styles.tendo}>
+        <div data-anim-target="left-joycon" className={styles['left-joycon']}>
+          <button
+            data-anim-target="button-x"
+            className={`${styles['btn-joycon-1']} ${styles['btn-up']}`}
+            onClick={() => {
+              actions?.current?.['onControlXClick']();
+            }}
+          >
+            <div className={styles.arrow}></div>
+          </button>
+          <button
+            data-anim-target="button-y"
+            className={`${styles['btn-joycon-1']} ${styles['btn-down']}`}
+            onClick={() => {
+              actions?.current?.['onControlYClick']();
+            }}
+          >
+            <div className={styles.arrow}></div>
+          </button>
+        </div>
 
-      <div
-        data-anim-target="controller-group"
-        className={styles['btn-group-bot']}
-      >
-        <button
-          data-anim-target="button-b"
-          className={`${styles['btn-joycon-2']} ${styles['btn-b']}`}
-          onClick={() => {
-            toSingleConModeAnimation.current?.play();
-            actions?.current?.['onControlBClick']();
-          }}
-        >
-          <div className={styles.letter}>B</div>
-        </button>
-        <button
-          data-anim-target="button-a"
-          className={`${styles['btn-joycon-2']} ${styles['btn-a']}`}
-          onClick={() => {
-            toSingleConModeAnimation.current?.reverse();
+        <div data-anim-target="screen" className={styles.screen}>
+          <div
+            data-anim-target="inner-screen"
+            className={styles['inner-screen']}
+          >
+            <div
+              data-anim-target="screen-foreground"
+              className={styles['work-landing-screen']}
+            >
+              <h3 className={styles.title}>Work</h3>
+              <div className={styles.guide}>
+                <span data-anim-target="guide-text">Press B to start</span>
+                <div
+                  data-anim-target="loading-bar"
+                  className={`${styles['loading-bar']} invisible`}
+                >
+                  <div
+                    data-anim-target="loading-progress"
+                    className={styles['inner-loading-bar']}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-            actions?.current?.['onControlAClick']();
-          }}
-        >
-          <div className={styles.letter}>A</div>
-        </button>
+        <div data-anim-target="right-joycon" className={styles['right-joycon']}>
+          <button
+            data-anim-target="button-a"
+            className={`${styles['btn-joycon-2']} ${styles['btn-a']}`}
+            onClick={() => {
+              actions?.current?.['onControlAClick']();
+            }}
+          >
+            <div className={styles.letter}>A</div>
+          </button>
+          <button
+            data-anim-target="button-b"
+            className={`${styles['btn-joycon-2']} ${styles['btn-b']}`}
+            onClick={() => {
+              actions?.current?.['onControlBClick']();
+            }}
+          >
+            <div className={styles.letter}>B</div>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/ControllerButton/useControllerAnimations.hook.ts
+++ src/components/ControllerButton/useControllerAnimations.hook.ts
@@ -2,65 +2,124 @@ import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 
 function useControllerAnimations() {
-  const toDetachedConModeAnimation = useRef<gsap.core.Timeline | null>(null);
-  const toSingleConModeAnimation = useRef<gsap.core.Timeline | null>(null);
-  const toAttachedMode = useRef<gsap.core.Timeline | null>(null);
+  const toSingleConModeTransition = useRef<gsap.core.Timeline | null>(null);
+  const toHandheldModeTransition = useRef<gsap.core.Timeline | null>(null);
+  const toDetachedModeTransition = useRef<gsap.core.Timeline | null>(null);
 
   useEffect(() => {
-    toDetachedConModeAnimation.current = toDetachedConMode();
-    toSingleConModeAnimation.current = toSingleConMode();
-    toAttachedMode.current = toSingleMode();
+    toSingleConModeTransition.current = toSingleConMode();
+    toHandheldModeTransition.current = toHandheldMode();
+    toDetachedModeTransition.current = toDetachedMode();
   }, []);
 
-  function toSingleMode(): gsap.core.Timeline {
+  function setScreenFromBelow(): gsap.core.Timeline {
+    const q = gsap.utils.selector('[data-anim-target="controller-button"]');
+    const screen = q('[data-anim-target="screen"]');
+    const guideText = q('[data-anim-target="guide-text"]');
+    const loadingBar = q('[data-anim-target="loading-bar"]');
+    const loadingProgress = q('[data-anim-target="loading-progress"]');
+    const screenForeground = q('[data-anim-target="screen-foreground"]');
+    const innerScreen = q('[data-anim-target="inner-screen"]');
+
+    return gsap
+      .timeline()
+      .set(screen, {
+        clearProps: 'all'
+      })
+      .set(screen, {
+        translateY: 250
+      })
+      .set(
+        guideText,
+        {
+          clearProps: 'all'
+        },
+        0
+      )
+      .set(
+        loadingBar,
+        {
+          clearProps: 'all'
+        },
+        0
+      )
+      .set(
+        screenForeground,
+        {
+          clearProps: 'all'
+        },
+        0
+      )
+      .set(
+        innerScreen,
+        {
+          clearProps: 'all'
+        },
+        0
+      )
+      .set(
+        loadingProgress,
+        {
+          clearProps: 'all'
+        },
+        0
+      );
+  }
+
+  function toHandheldMode(): gsap.core.Timeline {
+    const q = gsap.utils.selector('[data-anim-target="controller-button"]');
+    const screen = q('[data-anim-target="screen"]');
+
+    return gsap
+      .timeline({
+        paused: true
+      })
+      .add(setScreenFromBelow())
+      .addLabel('detached')
+      .to(q('[data-anim-target="left-joycon"]'), {
+        clearProps: 'all',
+        translateY: 0,
+        translateX: -125,
+        rotate: 0
+      })
+      .to(
+        q('[data-anim-target="right-joycon"]'),
+        {
+          clearProps: 'all',
+          translateY: 0,
+          translateX: 125,
+          rotate: 0
+        },
+        '<'
+      )
+      .to(screen, {
+        translateY: 0,
+        ease: 'back.out',
+        duration: 0.75
+      })
+      .addLabel('handheld');
+  }
+
+  function gatherCon(): gsap.core.Timeline {
     const q = gsap.utils.selector('[data-anim-target="controller-button"]');
 
     return gsap
       .timeline({
-        paused: true,
         defaults: {
-          delay: 1.5,
           duration: 0.5
         }
       })
-      .to(q('[data-anim-target="controller-group"]:nth-child(1)'), {
-        top: 0,
-        translateY: 0,
-        translateX: 0,
-        rotate: -40,
-        gap: 20
+      .to(q('[data-anim-target="left-joycon"]'), {
+        translateY: -26,
+        translateX: -30,
+        rotate: 50
       })
       .to(
-        q('[data-anim-target="controller-group"]:nth-child(2)'),
+        q('[data-anim-target="right-joycon"]'),
         {
-          top: 0,
-          translateY: 0,
-          translateX: 0,
-          rotate: -40,
-          gap: 20
-        },
-        '<'
-      );
-  }
-
-  function toDetachedConMode(): gsap.core.Timeline {
-    const q = gsap.utils.selector('[data-anim-target="controller-button"]');
-
-    return gsap
-      .timeline({ paused: true })
-      .to(q('[data-anim-target="controller-group"]:nth-child(1)'), {
-        top: '-30%',
-        translateY: -5,
-        translateX: '-22.5%',
-        rotate: -40
-      })
-      .to(
-        q('[data-anim-target="controller-group"]:nth-child(2)'),
-        {
-          top: '30%',
-          translateY: 5,
-          translateX: '22.5%',
-          rotate: -40
+          translateY: 26,
+          translateX: 35,
+          rotate: 50
         },
         '<'
       );
@@ -77,7 +136,7 @@ function useControllerAnimations() {
         right: 40
       })
       .to(
-        q('[data-anim-target="controller-group"]:nth-child(1)'),
+        q('[data-anim-target="left-joycon"]'),
         {
           top: 0,
           translateY: 0,
@@ -88,7 +147,7 @@ function useControllerAnimations() {
         '<'
       )
       .to(
-        q('[data-anim-target="controller-group"]:nth-child(2)'),
+        q('[data-anim-target="right-joycon"]'),
         {
           top: 0,
           translateY: 0,
@@ -99,10 +158,140 @@ function useControllerAnimations() {
       );
   }
 
+  function screenLoading(): gsap.core.Timeline {
+    const q = gsap.utils.selector('[data-anim-target="screen"]');
+    const guideText = q('[data-anim-target="guide-text"]');
+    const loadingBar = q('[data-anim-target="loading-bar"]');
+    const loadingProgress = q('[data-anim-target="loading-progress"]');
+    const screenForeground = q('[data-anim-target="screen-foreground"]');
+    const innerScreen = q('[data-anim-target="inner-screen"]');
+
+    return gsap
+      .timeline({
+        defaults: {
+          duration: 0.5
+        }
+      })
+      .to(guideText, {
+        autoAlpha: 0
+      })
+      .to(loadingBar, {
+        autoAlpha: 1
+      })
+      .to(loadingProgress, {
+        translateX: 0
+      })
+      .to(screenForeground, {
+        autoAlpha: 0
+      })
+      .to(
+        innerScreen,
+        {
+          background: '#fff'
+        },
+        '<'
+      );
+  }
+
+  function toDetachedMode(): gsap.core.Timeline {
+    const q = gsap.utils.selector('[data-anim-target="controller-button"]');
+    const screen = q('[data-anim-target="screen"]');
+    const con1 = q('[data-anim-target="left-joycon"]');
+    const con2 = q('[data-anim-target="right-joycon"]');
+
+    return gsap
+      .timeline({ paused: true })
+      .addLabel('handheld')
+      .add(screenLoading())
+      .to(con1, {
+        translateY: -25,
+        duration: 0.5,
+        ease: 'back.out'
+      })
+      .to(
+        con2,
+        {
+          translateY: 25,
+          duration: 0.5,
+          ease: 'back.out'
+        },
+        '<'
+      )
+      .to(
+        con1,
+        {
+          translateY: -25,
+          translateX: -145,
+          rotate: -15
+        },
+        '>-0.1'
+      )
+      .to(
+        con2,
+        {
+          translateX: 145,
+          rotate: 15
+        },
+        '<'
+      )
+      .to(
+        screen,
+        {
+          scale: 20,
+          transformOrigin: 'center 75%',
+          autoAlpha: 0,
+          duration: 1.5
+        },
+        '<1'
+      )
+      .add(gatherCon())
+      .add(setScreenFromBelow())
+      .addLabel('detached');
+  }
+
+  function enterDetachedMode(): void {
+    const controller = document.querySelector(
+      '[data-anim-target="controller-button"]'
+    );
+    toDetachedModeTransition?.current
+      ?.seek('detached')
+      .fromTo(
+        controller,
+        {
+          translateY: 250
+        },
+        {
+          translateY: 0,
+          duration: 1.25,
+          ease: 'back.out'
+        }
+      )
+      .play();
+  }
+
+  function enterHandheldMode(): void {
+    const controller = document.querySelector(
+      '[data-anim-target="controller-button"]'
+    );
+    gsap.timeline().fromTo(
+      controller,
+      {
+        translateY: 250
+      },
+      {
+        translateY: 0,
+        duration: 1.25,
+        ease: 'back.out'
+      }
+    );
+  }
+
   return {
-    toDetachedConModeAnimation,
-    toSingleConModeAnimation,
-    toAttachedMode
+    toSingleConModeTransition,
+    toHandheldModeTransition,
+    toDetachedModeTransition,
+    enterDetachedMode,
+    enterHandheldMode
   };
 }
 
