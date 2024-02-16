2164b7ae68282add2b657b3511f453c6f03a179a
Alvaro <alvarolukmanto@gmail.com>
2024-02-16 14:16:17 +0800

    ui - prevent broken layout and animations when tablet and mobile users rotate their screen
    force rerender the router when screen changes to desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.scss';
 import ControllerButton from './components/ControllerButton';
 import { ControllerButtonProvider } from './context/ControllerButtonContext';
+import useMediaQuery from './hooks/useMediaQuery.hook';
 import Router from './router/Router';
+import { MediaQueryScreen } from './utils/enums';
 
 function App() {
+  const isDesktop = useMediaQuery(MediaQueryScreen.Desktop);
+  const routerKey = isDesktop ? 'landscape-mode' : 'portrait-mode';
+
   return (
     <ControllerButtonProvider>
       <div className="app">
-        <Router />
+        <Router key={routerKey} />
         <ControllerButton />
       </div>
     </ControllerButtonProvider>
