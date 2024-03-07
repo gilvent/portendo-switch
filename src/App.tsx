141d4daea84a14ac7e6cc9ec06a1c235798859a7
Alvaro <alvarolukmanto@gmail.com>
2024-03-07 13:51:53 +0800

    perf - preload gaban font to avoid FOUC when navigating to work screen in home page
    FOUC = flash of unstyled content

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,23 @@ import { ControllerButtonProvider } from './context/ControllerButtonContext';
 import useMediaQuery from './hooks/useMediaQuery.hook';
 import Router from './router/Router';
 import { MediaQueryScreen } from './utils/enums';
+import WebFont from 'webfontloader';
 
 function App() {
   const isDesktop = useMediaQuery(MediaQueryScreen.Desktop);
   const routerKey = isDesktop ? 'landscape-mode' : 'portrait-mode';
   const controllerKey = 'controller-' + routerKey;
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Fredoka:400,500,600,700']
+      },
+      custom: {
+        families: ['Gaban']
+      }
+    });
+  }, []);
 
   useEffect(() => {
     const listener = () => {
